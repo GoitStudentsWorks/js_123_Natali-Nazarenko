27aefe5aaf822212a957736715835ea18c16926a
AnnaM <amykhailova84@gmail.com>
2026-02-08 23:44:12 -0600

    fix bio

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,1 @@
 import './render-functions/render-modal';
-
-const modalEl = document.querySelector('section.modal');
-console.log(modalEl);
-
-const closeBtn = modalEl.querySelector('.modal-btn');
-console.log(closeBtn);
-
-closeBtn.addEventListener('click', modalCloseBtn);
-
-function modalCloseBtn(e) { 
-  modalEl.classList.toggle('is-open');
-};
